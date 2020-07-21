2f2d6e99ae385f1edaea6c0a2d0f38ddd6963f35
Nathan T. James <nj1154@gmail.com>
2020-07-20 22:36:29 -0500

    shell of script to compare CPM implementations

---
+++ comp_implement.R
@@ -0,0 +1,25 @@
+# compare ordinal models
+
+
+# bayesCPM 
+
+
+
+# bayesCPM w/ QR decomp?
+
+
+# for goodrich mods see https://github.com/bgoodri/covid19_Bayesian_RCT
+# goodrich w/o QR decomp
+## ord_mod_goodrich.stan, same as ordered_logit.stan from above
+
+# goodrich w/ QR decomp (blrm?)
+## lrmqr.stan from above
+
+# betancourt w/o QR decomp
+## ord_mod_betancourt.stan
+
+# betancourt w/ QR decomp
+
+
+# OpenBUGS - see /home/nathan/Dropbox/njames/stat/stat_book_exercises/congdon_bayes_for_cat_data
+
