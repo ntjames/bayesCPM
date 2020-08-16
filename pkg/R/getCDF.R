8c5eb19b7ea2fdafcf108df93b8bfc31a9782b34
Nathan T. James <nj1154@gmail.com>
2020-08-15 20:09:04 -0500

    update functions and documentation for new bayes_cpm() output

--- pkg/R/getCDF.R
+++ pkg/R/getCDF.R
@@ -1,100 +1,98 @@
 #' @title Get conditional CDF from fit Bayes CPM model
 #'
 #' @description Calculate the posterior conditional CDF from a Bayes CPM fit
-#' @param fit 'stanfit' object
-#' @param fitdata list containing data used to fit model
+#' @param fit output list from bayes_cpm() containing 'stanfit' object and 'standata' data used to fit model
 #' @param newdata a data frame with columns for each predictor used in the model
 #' @param summ logical. Should the function return a summary of the posterior
 #'    conditional CDF? (default=TRUE)
 #' @return posterior conditional CDF summary (summ=TRUE) or values (summ=FALSE)
 #' @examples
-#' fit <- bayes_cpm(dat1_stan)
-#' getCDF(fit, dat1_stan, data.frame(pred1=c(0,1),pred2=c(1,1)))
+#' fit <- bayes_cpm(y~x1+x2, data=dat)
+#' getCDF(fit, data.frame(x1=c(0,1),x2=c(1,1)))
 
 #' @export
-getCDF <- function(fit, fitdata, newdata, summ=TRUE, ...){
-  #! require(stringr) #is stringr used??
-
+getCDF <- function(fit, newdata, summ=TRUE, ...){
+  
   #check that cumulative model used?
-
+  
   #check for stanfit and newdata
-  if( !identical(class(fit)[1],"stanfit")) stop("fit must be `stanfit` object")
-
+  if( !identical(class(fit$stanfit)[1],"stanfit")) stop("fit must be `stanfit` object")
+  
   #! check newdata is a data.frame
-
+  
   # check that names in newdata match coefs from model
-  if( !identical(sort(names(fitdata$Q)),
+  if( !identical(sort(names(fit$standata$Q)),
                  sort(names(newdata))) ) stop("newdata vars must match model vars")
-
+  
   #! other checks
-
+  
   # get data.frame with posterior samples
-  posts <- as.data.frame(fit)
-
+  posts <- as.data.frame(fit$stanfit)
+  
   nsamps <- nrow(posts)
-
+  
   # get ordered values of outcome
   #!truey0 <- fitdata$truey0
   #!truey0levs <- fitdata$ncat
-
+  
   # prepend value less than min(y) for alpha_0=-Inf intercept
-  truey <- c(-Inf,fitdata$truey0)
-
+  truey <- c(-Inf,fit$standata$truey0)
+  
   # format newdata, betas, and intercepts
   #!npreds <- fit@par_dims$b
-  cv_nms <- paste0("b[",1:fit@par_dims$b,"]")
-  cut_nms <- paste0("cutpoints[",1:(fitdata$ncat-1),"]")
-
+  cv_nms <- paste0("b[",1:fit$stanfit@par_dims$b,"]")
+  cut_nms <- paste0("cutpoints[",1:(fit$standata$ncat-1),"]")
+  
   ndr <- newdata %>% dplyr::mutate(ndrow=1:n())
   nd <- ndr %>% dplyr::select(-ndrow) %>% as.matrix()
-
-  beta <- posts %>% dplyr::select(cv_nms) %>% as.matrix()
-  int <- posts %>% dplyr::select(cut_nms) %>% as.matrix()
-
+  
+  beta <- posts %>% dplyr::select(all_of(cv_nms)) %>% as.matrix()
+  int <- posts %>% dplyr::select(all_of(cut_nms)) %>% as.matrix()
+  
   # get matrix of linear predictions Xb
   # (rxp) x (pxs) = rxs
   # r is rows in newdata, p is parameters (cols) in newdata,
   # s is number of MCMC samples
   # below is equivalent to Xb <- nd %*% t(beta), but faster
   Xb <- Matrix::tcrossprod(nd,beta)
-
+  
   # add Xb to each intercept (4000xints)
   # dim(int) => s x (ints-1)
   # dim(Xb) => r x s
-
+  
   # use inverse function based on family
   # 1 = logistic; 2 = probit; 3 = loglog; 4 = cloglog; 5 = cauchit !check cauchit
-  inv_func <- switch(fitdata$link,
+  inv_func <- switch(fit$standata$link,
                      plogis,
                      pnorm,
                      function(y) exp(-exp(-y)),
                      function(y) 1-exp(-exp(y)),
                      pcauchy)
-
+  
   #will have 1 for each row of nd
   #! check model/doc to make sure values are being calculated correctly
   #! are cutpoints y<= or y< ??
-
+  
   nci <- ncol(int)
   nrnd <- nrow(nd)
-
+  
   nd_lst <- vector("list", length=nrnd)
   for (i in 1:nrnd){
     #  F(y|X)=G^-1(alpha_i-betaX)
     tmpcdf0 <- int - t(Xb[rep(i,nci),, drop=FALSE])
-
+    
     # add alpha_0=-Inf and alpha_n = Inf, convert to data.frame.table, get cdf
     nd_lst[[i]] <- cbind(`-Inf`=-Inf, tmpcdf0, `Inf`=Inf) %>%
       as.data.frame.table() %>%
       dplyr::mutate(cdf=inv_func(Freq), ndrow=i) %>%
       cbind(nd[i,,drop=FALSE])
   }
-
+  
   # combine conditional cdfs
   #! cdf_vals <- do.call(rbind, lapply(nd_ds, function(x) get(as.character(x))))
   #! cdf_vals <- bind_rows( lapply(nd_ds, function(x) get(as.character(x))) )
   cdf_vals <- bind_rows( nd_lst )
-
+  
   if (summ){
     cdf_summ <- cdf_vals %>%
       ungroup() %>%
@@ -108,8 +106,9 @@ getCDF <- function(fit, fitdata, newdata, summ=TRUE, ...){
                        cdf_q75=quantile(cdf,probs=0.75),
                        cdf_q90=quantile(cdf,probs=0.90),
                        cdf_q95=quantile(cdf,probs=0.95),
-                       cdf_q97.5=quantile(cdf,probs=0.975)) %>%
-      ungroup() %>% dplyr::mutate(yval=rep(truey,nrow(nd))) %>%
+                       cdf_q97.5=quantile(cdf,probs=0.975), .groups="drop") %>%
+      ungroup() %>% # redundant w/ .groups="drop"?
+      dplyr::mutate(yval=rep(truey,nrow(nd))) %>%
       full_join(., ndr, by="ndrow")
     return(cdf_summ)
   } else {
@@ -119,5 +118,5 @@ getCDF <- function(fit, fitdata, newdata, summ=TRUE, ...){
       dplyr::mutate(yval=rep(truey,nrnd*nsamps))
     return(cdf_out)
   }
-
+  
 }
--- pkg/R/getMean.R
+++ pkg/R/getMean.R
@@ -1,28 +1,27 @@
 #' @title Get conditional mean from fit Bayes CPM model
 #'
 #' @description Calculate the posterior conditional mean from a Bayes CPM fit
-#' @param fit 'stanfit' object
-#' @param fitdata list containing data used to fit model
+#' @param fit output list from bayes_cpm() containing 'stanfit' object and 'standata' data used to fit model
 #' @param newdata a data frame with columns for each predictor used in the model
 #' @param summ logical. Should the function return a summary of the posterior
 #'    conditional mean? (default=TRUE)
 #' @return posterior conditional mean summary (summ=TRUE) or values (summ=FALSE)
 #' @examples
-#' fit <- bayes_cpm(dat1_stan)
-#' getMean(fit, dat1_stan, data.frame(pred1=c(0,1),pred2=c(1,1)))
+#' fit <- bayes_cpm(y~x1+x2, data=dat)
+#' getMean(fit, data.frame(x1=c(0,1),x2=c(1,1)))
 
 #' @export
-getMean <- function(fit, fitdata, newdata, summ=TRUE,...){
-
-  cdf_samps <- getCDF(fit, fitdata, newdata, summ=FALSE)
-
+getMean <- function(fit, newdata, summ=TRUE,...){
+  
+  cdf_samps <- getCDF(fit, newdata, summ=FALSE)
+  
   mn_vals <- cdf_samps %>% dplyr::filter(cdf!=0) %>% group_by(ndrow, Var1) %>%
     mutate(pdf0=lag(cdf), pdf=ifelse(is.na(pdf0),cdf,cdf-pdf0), fy_Py=pdf*yval) %>%
-    dplyr::summarize(n=n(),mn=sum(fy_Py)) %>% ungroup()
-
+    dplyr::summarize(n=n(),mn=sum(fy_Py),.groups="drop")
+  
   #! pass this from getCDF rather than recreating?
   ndr <- newdata %>% dplyr::mutate(ndrow=1:n())
-
+  
   if (summ){
     mn_summ<-mn_vals %>%
       ungroup() %>%
@@ -37,7 +36,7 @@ getMean <- function(fit, fitdata, newdata, summ=TRUE,...){
                        mn_q75=quantile(mn,probs=0.75),
                        mn_q90=quantile(mn,probs=0.90),
                        mn_q95=quantile(mn,probs=0.95),
-                       mn_q97.5=quantile(mn,probs=0.975)) %>%
+                       mn_q97.5=quantile(mn,probs=0.975), .groups="keep") %>%
       full_join(., ndr, by="ndrow")
     return(mn_summ)
   } else {
@@ -45,5 +44,45 @@ getMean <- function(fit, fitdata, newdata, summ=TRUE,...){
     mn_vals_app<-mn_vals %>% full_join(., ndr, by="ndrow")
     return(mn_vals_app)
   }
-
+  
 }
+
+
+
+# getMean <- function(fit, fitdata, newdata, summ=TRUE,...){
+# 
+#   cdf_samps <- getCDF(fit, fitdata, newdata, summ=FALSE)
+# 
+#   mn_vals <- cdf_samps %>% dplyr::filter(cdf!=0) %>% group_by(ndrow, Var1) %>%
+#     mutate(pdf0=lag(cdf), pdf=ifelse(is.na(pdf0),cdf,cdf-pdf0), fy_Py=pdf*yval) %>%
+#     dplyr::summarize(n=n(),mn=sum(fy_Py)) %>% ungroup()
+# 
+#   #! pass this from getCDF rather than recreating?
+#   ndr <- newdata %>% dplyr::mutate(ndrow=1:n())
+# 
+#   if (summ){
+#     mn_summ<-mn_vals %>%
+#       ungroup() %>%
+#       group_by(ndrow) %>%
+#       dplyr::summarize(mean_mn=mean(mn),
+#                        med_mn=median(mn),
+#                        sd_mn=sd(mn),
+#                        mn_q2.5=quantile(mn,probs=0.025),
+#                        mn_q5=quantile(mn,probs=0.05),
+#                        mn_q10=quantile(mn,probs=0.10),
+#                        mn_q25=quantile(mn,probs=0.25),
+#                        mn_q75=quantile(mn,probs=0.75),
+#                        mn_q90=quantile(mn,probs=0.90),
+#                        mn_q95=quantile(mn,probs=0.95),
+#                        mn_q97.5=quantile(mn,probs=0.975)) %>%
+#       full_join(., ndr, by="ndrow")
+#     return(mn_summ)
+#   } else {
+#     #append variable info
+#     mn_vals_app<-mn_vals %>% full_join(., ndr, by="ndrow")
+#     return(mn_vals_app)
+#   }
+# 
+# }
+
+
